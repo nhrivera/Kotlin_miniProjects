7713c0746834080343426b98c9fc14eacd4999ef
Nestor <nhrivera98@gmail.com>
2020-07-30 08:25:53 -0500

    mathMagic

---
+++ src/mathMagic.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val userName = "Nestor"
+    // magicNum is using an arithmetic magic trick to determine my age
+    var magicNum = (2 * 5 + 3) * 2 + 1 - 6
+    println("$userName's age is $magicNum.")
+}
